b2d50e87d9c7f07ee0d5c9e0abb9d7a74128c03d
luis lewis <siwelsiul@gmail.com>
2015-03-26 20:43:06 +0100

    ls colors and options

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,1 +1,3 @@
 alias tmux="tmux -f ~/.config/tmux.conf"
+
+alias ls='ls --color=auto'
--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -1,1 +1,8 @@
 export PATH=$PATH:/home/miguel/bin
+
+# Setup terminal, and turn on colors
+export TERM=xterm-256color
+
+export LESS='--ignore-case --raw-control-chars'
+export PAGER='less'
+export EDITOR='vim'
