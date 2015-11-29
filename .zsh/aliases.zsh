b5be496a02836b265c4c642203f4fd851c685fd3
siwelsiul <siwelsiul@gmail.com>
2015-11-29 21:14:46 +0100

    Translation aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -3,3 +3,7 @@ alias tmux="tmux -f ~/.config/tmux.conf"
 alias ls='ls --color=auto'
 alias mpv='mpv --fs'
 alias yt='youtube-viewer --resolution=360p'
+
+alias trde='trans -s de -t es -I'
+alias tres='trans -s es -t de -I'
+alias tren='trans -s en -t es -I'
