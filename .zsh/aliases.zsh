afaa5fa4aa4566f40862f44c92bff4106221102b
siwelsiul <siwelsiul@gmail.com>
2015-11-29 22:20:19 +0100

    merge solved

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -3,12 +3,10 @@ alias tmux="tmux -f ~/.config/tmux.conf"
 alias ls='ls --color=auto'
 alias mpv='mpv --fs'
 alias yt='youtube-viewer --resolution=360p'
-<<<<<<< HEAD
 
 alias trde='trans -s de -t es -I'
 alias tres='trans -s es -t de -I'
 alias tren='trans -s en -t es -I'
-=======
+
 alias yt='youtube-viewer --resolution=360p -S'
 alias p='ping -c 3 8.8.8.8'
->>>>>>> 5484a0af89877d54fb1d5930b7d9f944aae9e00e
